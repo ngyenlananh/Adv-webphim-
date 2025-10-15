6ce177350da78de26d57622eef1b9c734faffb5a
ngyenlananh <23010823@st.phenikaa-uni.edu.vn>
2025-10-15 15:34:57 +0700

    thêm các controller

--- web_phim.sql
+++ web_phim.sql
@@ -1,31 +1,11 @@
--- phpMyAdmin SQL Dump
--- version 5.1.3
--- https://www.phpmyadmin.net/
---
--- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th5 05, 2022 lúc 12:44 PM
--- Phiên bản máy phục vụ: 10.4.24-MariaDB
--- Phiên bản PHP: 7.4.28
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
---
--- Cơ sở dữ liệu: `web_phim`
---
-
--- --------------------------------------------------------
-
---
--- Cấu trúc bảng cho bảng `categories`
---
 
 CREATE TABLE `categories` (
   `id` int(11) NOT NULL,
